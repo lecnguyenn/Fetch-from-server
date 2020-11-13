5e067bc49e3ed52cdc5af264775d24cb7d414c89
Nguyen Le <69196079+lecnguyenn@users.noreply.github.com>
2020-11-13 16:17:33 +0700

    Create Maincomponent.js

---
+++ Fetch Post Comment/Maincomponent.js
@@ -0,0 +1,9 @@
+import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
+
+. . .
+
+  postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))
+
+. . .
+
+            postComment={this.props.postComment}
