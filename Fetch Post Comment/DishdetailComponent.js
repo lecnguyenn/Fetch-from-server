0260b5f911eddba53bd6ddb7121635e1c875cff6
Nguyen Le <69196079+lecnguyenn@users.noreply.github.com>
2020-11-13 16:17:50 +0700

    Create DishdetailComponent.js

---
+++ Fetch Post Comment/DishdetailComponent.js
@@ -0,0 +1,14 @@
+ function RenderComments({comments, postComment, dishId}) {
+      
+. . .
+
+                    <CommentForm dishId={dishId} postComment={postComment} />
+                    
+. . .
+
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
+            
+. . .
+
+                            postComment={props.postComment}
+                            
